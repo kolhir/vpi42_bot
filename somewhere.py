52f910552066f596185c6b1cdb0afde973e391d3
psevdoworld <psevdoworld@yandex.ru>
2018-02-16 15:51:59 +0300

    Update somewhere.py

--- somewhere.py
+++ somewhere.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from telebot import types
-token = '' #test4
-admins=('148964502','65353297')
-alive_message="...em lliK lol kek cheboureck "
+token = 'your token here'
+admins=('admin','another admin')
+alive_message="kill me please "
 def getadmins():
 	return(admins)
 
 def i_am_alive(bot):
     keyboard = types.InlineKeyboardMarkup()
-    callback_button = types.InlineKeyboardButton(text="git gud", callback_data="test")
+    callback_button = types.InlineKeyboardButton(text="alive button", callback_data="alive data")
     keyboard.add(callback_button)
     for admin in admins: bot.send_message(admin, alive_message, parse_mode='MARKDOWN',reply_markup=keyboard)
