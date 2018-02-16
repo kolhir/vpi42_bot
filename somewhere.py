b476ee6703927b43b30ae804d5778f413ade2909
psevdoworld <psevdoworld@yandex.ru>
2018-02-16 16:16:17 +0300

    Delete somewhere.py

--- somewhere.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-token = 'YOUR TOKEN HERE'
-admins=('your admin','another admin')
-alive_message="`   рил толк`\n` я снова жив`\n  __но это не точно__ "
-def getadmins():
-	return(admins)
-
-def i_am_alive(bot):
-    for admin in admins: bot.send_message(admin, alive_message, parse_mode='MARKDOWN')
